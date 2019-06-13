6cc6ef6df63307ebfa7dc7c749e32505739fbbe2
Mardloq <mardloq110@yandex.ru>
2019-06-13 13:49:19 +0300

    Коммит

---
+++ ccccc/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+let persone = {
+    name: "Fred",
+    age: 25,
+    Loh: "da"
+}
+
+
+console.log(persone.age);
+
+let arr = ['plum.png','orange.jpg','apple.bmp']
+console.log(arr[1]);
+
+//alert("Hello world")
+//let answer = confirm("Кирюха лох?");
+
+//console.log(answer);
+
+// let answer = prompt("ВЫ лох?", "Да");
+
+// console.log(typeof(answer));
+// console.log("stroka" + " - object")
+// console.log(4 + " - object")
+
+let incr = 10,
+    decr = 10;
+    
+incr++;
+++decr;
+console.log(++incr,++decr);
+
+console.log(5**2);
+console.log(2 == 2);
+
+let ischecked = true,
+    isclose = false;
+    
+
+console.log(ischecked || isclose);
+
+var f = 1.25;
+console.log(typeof(f));
